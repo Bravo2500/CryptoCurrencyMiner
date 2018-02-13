a5f4b2c0cf6a6ce31b02f5f8affc16d3bf8e1e5c
digitaldisarray <electrichexagon8@gmail.com>
2018-02-12 16:54:56 -0800

    :shit: Very bad gui

--- CryptoCurrencyMiner/CryptoCurrencyMiner.Designer.cs
+++ CryptoCurrencyMiner/CryptoCurrencyMiner.Designer.cs
@@ -28,22 +28,111 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.IpColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HashColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Crypto Currency Miner Server Alpha";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.IpColumn,
+            this.NameColumn,
+            this.HashColumn});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 236);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellClick);
+            // 
+            // IpColumn
+            // 
+            this.IpColumn.HeaderText = "IP";
+            this.IpColumn.Name = "IpColumn";
+            this.IpColumn.ReadOnly = true;
+            // 
+            // NameColumn
+            // 
+            this.NameColumn.HeaderText = "Host Name";
+            this.NameColumn.Name = "NameColumn";
+            this.NameColumn.ReadOnly = true;
+            this.NameColumn.Width = 200;
+            // 
+            // HashColumn
+            // 
+            this.HashColumn.HeaderText = "Hash Rate";
+            this.HashColumn.Name = "HashColumn";
+            this.HashColumn.ReadOnly = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 271);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Builder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop Server";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // CryptoCurrencyMiner
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(461, 335);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.ClientSize = new System.Drawing.Size(469, 306);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "CryptoCurrencyMiner";
-            this.Text = "CryptoCurrencyMiner";
+            this.Text = "Crypto Currency Miner Server";
             this.Load += new System.EventHandler(this.CryptoCurrencyMiner_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn IpColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HashColumn;
     }
 }
 
--- CryptoCurrencyMiner/CryptoCurrencyMiner.cs
+++ CryptoCurrencyMiner/CryptoCurrencyMiner.cs
@@ -19,7 +19,37 @@ namespace CryptoCurrencyMiner
 
         private void CryptoCurrencyMiner_Load(object sender, EventArgs e)
         {
+            // https://msdn.microsoft.com/en-us/library/system.windows.forms.contextmenu(v=vs.110).aspx
+            // Set the dataGridView's context menu
+            ContextMenu cm = new ContextMenu();
+            cm.MenuItems.Add("Start Mining");
+            cm.MenuItems.Add("Stop Mining");
+            cm.MenuItems.Add("Client Settings");
+            cm.MenuItems.Add("Disconnect"); // Make sure it asks "Are you sure?"
 
+            dataGridView1.ContextMenu = cm;
+
+            // Add test data
+            dataGridView1.Rows.Add("127.0.0.1", "Test", "240");
+
+            // Start Server
+            
+        }
+
+        private void DataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Show the context menu at current mouse position
+            dataGridView1.ContextMenu.Show(this, new Point(MousePosition.X, MousePosition.Y));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Stop the server
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Open builder window
         }
     }
 }
